8168c3b386457829e32fbca571b321fd937e3214
868 <868@aksteel>
2024-10-31 08:26:14 +0330

    add log classes

---
+++ Generic/Base/Handler/Log/Abstract/AbstractGenericLogHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic.Base.Handler.Log.Abstract
+{
+    internal class AbstractGenericLogHandler
+    {
+    }
+}
