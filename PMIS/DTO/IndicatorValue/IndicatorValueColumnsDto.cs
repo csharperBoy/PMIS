74807b0d3bbcacf5ba201d64e6fa9b6244abf08b
iran-haran <867@aksteel>
2024-12-02 08:34:46 +0330

    merge

---
+++ PMIS/DTO/IndicatorValue/IndicatorValueColumnsDto.cs
@@ -0,0 +1,245 @@
+﻿using PMIS.Bases;
+using PMIS.DTO.Indicator;
+using PMIS.DTO.LookUp.Info;
+using PMIS.DTO.LookUpDestination;
+using PMIS.DTO.LookUpValue.Info;
+using PMIS.Services.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PMIS.DTO.IndicatorValue
+{
+
+    public class IndicatorValueColumnsDto //: GenericColumnsDto
+    {
+        public async Task Initialize(ILookUpValueService lookUpValueService, IIndicatorService indicatorService)
+        {
+            IEnumerable<LookUpDestinationSearchResponseDto> lstLookUpDestinationIndicatorValues = await lookUpValueService.GetList("IndicatorValue");
+            IEnumerable<LookUpDestinationSearchResponseDto> lstLookUpDestinationIndicator = await lookUpValueService.GetList("Indicator");
+            //List<IndicatorSearchResponseDto> lstIndicator = new List<IndicatorSearchResponseDto>() { new IndicatorSearchResponseDto() { Id = 0 , Title = "همه"} };
+            (bool isSuccessInd, IEnumerable<IndicatorSearchResponseDto> lstIndicator) = await indicatorService.Search(new Generic.Service.DTO.Concrete.GenericSearchRequestDto()
+            {
+                filters = new List<Generic.Service.DTO.Concrete.GenericSearchFilterDto>(){
+                new Generic.Service.DTO.Concrete.GenericSearchFilterDto()
+                {
+                    columnName = "FlgLogicalDelete",
+                    LogicalOperator = Generic.Service.DTO.Concrete.LogicalOperator.And,
+                    operation = Generic.Service.DTO.Concrete.FilterOperator.Equals,
+                    type = Generic.Service.DTO.Concrete.PhraseType.Condition,
+                    value = false.ToString()
+                }
+                }
+            });
+            lstIndicator = (await indicatorService.SearchByExternaFilter(lstIndicator, GlobalVariable.userId));
+            // lstIndicator.AddRange(lstIndicator1);
+            var lstLkpShift = (await lookUpValueService.GetList(lstLookUpDestinationIndicatorValues, "FkLkpShiftID", "LkpShift"));
+            var lstLkpForm = (await lookUpValueService.GetList(lstLookUpDestinationIndicator, "FkLkpFormID", "LkpForm"));
+            var LkpPeriod = (await lookUpValueService.GetList(lstLookUpDestinationIndicator, "FkLkpPeriodID", "LkpPeriod"));
+            var lstLkpValueType = (await lookUpValueService.GetList(lstLookUpDestinationIndicatorValues, "FkLkpValueTypeID", "LkpValueType"));
+
+
+            FilterColumns.AddRange(new List<DataGridViewColumn>()
+            {
+
+               new DataGridViewTextBoxColumn()
+               {
+                   HeaderText = "از تاریخ",
+                   Name = "DateTimeFrom",
+                   DataPropertyName = "DateTimeFrom",
+                   ReadOnly = false,
+                   Visible = true,
+                  MinimumWidth = 100,
+                    DividerWidth = 5
+               },
+                new DataGridViewTextBoxColumn()
+               {
+                   HeaderText = "تا تاریخ",
+                   Name = "DateTimeTo",
+                   DataPropertyName = "DateTimeTo",
+                   ReadOnly = false,
+                   Visible = true,
+                   MinimumWidth = 100,
+                   DividerWidth = 5
+               },
+                new DataGridViewComboBoxColumn()
+               {
+                   HeaderText = (await lookUpValueService.GetList(lstLookUpDestinationIndicatorValues, "LkpShift")).Single().Title,
+                   Name = "FkLkpShiftId",
+                   DataPropertyName = "FkLkpShiftId",
+                   DisplayMember = "Display",
+                   ValueMember = "Id",
+                   DataSource =  (await lookUpValueService.GetList(lstLookUpDestinationIndicatorValues, "FkLkpShiftID", "LkpShift")).ToArray(),
+                   ReadOnly = false,
+                   Visible = true,
+                   MinimumWidth = 150,
+                   DividerWidth = 5
+               },new DataGridViewComboBoxColumn()
+               {
+                   HeaderText = (await lookUpValueService.GetList(lstLookUpDestinationIndicator, "LkpForm")).Single().Title,
+                   Name = "VrtLkpForm",
+                   DataPropertyName = "VrtLkpFormId",
+                   DisplayMember = "Display",
+                   ValueMember = "Id",
+                   DataSource =  (await lookUpValueService.GetList(lstLookUpDestinationIndicator, "FkLkpFormID", "LkpForm")).ToArray(),
+                   ReadOnly = false,
+                   Visible = true,
+                  MinimumWidth = 150,
+                   DividerWidth = 5
+               },
+               new DataGridViewComboBoxColumn()
+               {
+                   HeaderText = (await lookUpValueService.GetList(lstLookUpDestinationIndicator, "LkpPeriod")).Single().Title,
+                   Name = "VrtLkpPeriod",
+                   DataPropertyName = "VrtLkpPeriodId",
+                   DisplayMember = "Display",
+                   ValueMember = "Id",
+                   DataSource =  (await lookUpValueService.GetList(lstLookUpDestinationIndicator, "FkLkpPeriodID", "LkpPeriod")).ToArray(),
+                   ReadOnly = false,
+                   Visible = true,
+                   MinimumWidth = 150,
+                   DividerWidth = 5
+               },
+                new DataGridViewComboBoxColumn()
+               {
+                   HeaderText = "شاخص",
+                   Name = "FkIndicatorId",
+                   DataPropertyName = "FkIndicatorId",
+                   DisplayMember = "Title",
+                   ValueMember = "Id",
+                   DataSource = lstIndicator.ToArray(),
+                   ReadOnly = false,
+                   Visible = true,
+                   MinimumWidth = 150,
+                   DividerWidth = 5
+               },
+               new DataGridViewComboBoxColumn()
+               {
+                   HeaderText = (await lookUpValueService.GetList(lstLookUpDestinationIndicatorValues, "LkpValueType")).Single().Title,
+                   Name = "FkLkpValueTypeId",
+                   DataPropertyName = "FkLkpValueTypeId",
+                   DisplayMember = "Display",
+                   ValueMember = "Id",
+                   DataSource =  (await lookUpValueService.GetList(lstLookUpDestinationIndicatorValues, "FkLkpValueTypeID", "LkpValueType")).ToArray(),
+                   ReadOnly = false,
+                   Visible = true,
+                   MinimumWidth = 150,
+                   DividerWidth = 5
+               },
+               new DataGridViewTextBoxColumn()
+               {
+                   HeaderText = "توضیحات",
+                   Name = "Description",
+                   DataPropertyName = "Description",
+                   ReadOnly = false,
+                   Visible = true,
+                   MinimumWidth = 200,
+                   DividerWidth = 5
+               }
+        });
+
+            ResultColumns.AddRange(new List<DataGridViewColumn>()
+            {
+                new DataGridViewTextBoxColumn()
+                {
+                    HeaderText = "شناسه",
+                    Name = "Id",
+                    DataPropertyName = "Id",
+                    ReadOnly = true,
+                    Visible = false,
+                },
+                new DataGridViewTextBoxColumn()
+               {
+                   HeaderText = "تاریخ",
+                   Name = "shamsiDateTime",
+                   DataPropertyName = "shamsiDateTime",
+                   ReadOnly = true,
+                   Visible = true,
+                   Frozen = true,
+               },
+                new DataGridViewComboBoxColumn()
+               {
+                   HeaderText = (await lookUpValueService.GetList(lstLookUpDestinationIndicatorValues, "LkpShift")).Single().Title,
+                   Name = "FkLkpShiftId",
+                   DataPropertyName = "FkLkpShiftId",
+                   DisplayMember = "Display",
+                   ValueMember = "Id",
+                   DataSource =  (await lookUpValueService.GetList(lstLookUpDestinationIndicatorValues, "FkLkpShiftID", "LkpShift")).ToArray(),
+                   ReadOnly = true,
+                   Visible = true,
+                   Frozen = true,
+               },
+                new DataGridViewComboBoxColumn()
+               {
+                   HeaderText = (await lookUpValueService.GetList(lstLookUpDestinationIndicator, "LkpForm")).Single().Title,
+                   Name = "VrtLkpForm",
+                   DataPropertyName = "VrtLkpFormId",
+                   DisplayMember = "Display",
+                   ValueMember = "Id",
+                   DataSource =  (await lookUpValueService.GetList(lstLookUpDestinationIndicator, "FkLkpFormID", "LkpForm")).ToArray(),
+                   ReadOnly = true,
+                   Visible = false,
+               },
+                new DataGridViewComboBoxColumn()
+               {
+                   HeaderText = (await lookUpValueService.GetList(lstLookUpDestinationIndicator, "LkpPeriod")).Single().Title,
+                   Name = "VrtLkpPeriod",
+                   DataPropertyName = "VrtLkpPeriodId",
+                   DisplayMember = "Display",
+                   ValueMember = "Id",
+                   DataSource =  (await lookUpValueService.GetList(lstLookUpDestinationIndicator, "FkLkpPeriodID", "LkpPeriod")).ToArray(),
+                   ReadOnly = true,
+                   Visible = true,
+               },
+                new DataGridViewComboBoxColumn()
+               {
+                   HeaderText = "شاخص",
+                   Name = "FkIndicatorId",
+                   DataPropertyName = "FkIndicatorId",
+                   DisplayMember = "Title",
+                   ValueMember = "Id",
+                   DataSource = lstIndicator,
+                   ReadOnly = true,
+                   Visible = true,
+               },
+                new DataGridViewComboBoxColumn()
+               {
+                   HeaderText = (await lookUpValueService.GetList(lstLookUpDestinationIndicatorValues, "LkpValueType")).Single().Title,
+                   Name = "FkLkpValueTypeId",
+                   DataPropertyName = "FkLkpValueTypeId",
+                   DisplayMember = "Display",
+                   ValueMember = "Id",
+                   DataSource =  (await lookUpValueService.GetList(lstLookUpDestinationIndicatorValues, "FkLkpValueTypeID", "LkpValueType")).ToArray(),
+                   ReadOnly = true,
+                   Visible = true,
+               },
+                new DataGridViewTextBoxColumn()
+               {
+                   HeaderText = "مقدار",
+                   Name = "Value",
+                   DataPropertyName = "Value",
+                   ReadOnly = true,
+                   Visible = true,
+               },
+                new DataGridViewTextBoxColumn()
+               {
+                   HeaderText = "توضیحات",
+                   Name = "Description",
+                   DataPropertyName = "Description",
+                   ReadOnly = true,
+                   Visible = true,
+               }
+        });
+
+
+        }
+
+        public List<DataGridViewColumn> FilterColumns { get; set; } = new List<DataGridViewColumn>();
+        public List<DataGridViewColumn> ResultColumns { get; set; } = new List<DataGridViewColumn>();
+
+    }
+}
+
---
+++ PMIS/Services/LookUpValueService.cs
@@ -0,0 +1,119 @@
+﻿using AutoMapper;
+using Generic.Base.Handler.Map.Abstract;
+using Generic.Service.Normal.Composition;
+using Generic.Service.Normal.Operation.Abstract;
+using PMIS.DTO.LookUpValue;
+using PMIS.DTO.LookUp;
+using PMIS.DTO.LookUp.Info;
+using PMIS.DTO.LookUpValue;
+using PMIS.DTO.LookUpValue.Info;
+using PMIS.Models;
+using PMIS.Services.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PMIS.DTO.LookUpDestination;
+using Generic.Base.Handler.Map;
+using Generic.Service.DTO.Abstract;
+using Generic.Service.DTO.Concrete;
+using System.DirectoryServices.ActiveDirectory;
+
+namespace PMIS.Services
+{
+    public class LookUpValueService
+: GenericNormalService<PmisContext, LookUpValue, LookUpValueAddRequestDto, LookUpValueAddResponseDto, LookUpValueEditRequestDto, LookUpValueEditResponseDto, LookUpValueDeleteRequestDto, LookUpValueDeleteResponseDto, LookUpValueSearchResponseDto>
+       , ILookUpValueService
+    {
+        ILookUpDestinationService lookUpDestinationService;
+        public LookUpValueService(AbstractGenericMapHandler _mapper, AbstractGenericNormalAddService<PmisContext, LookUpValue, LookUpValueAddRequestDto, LookUpValueAddResponseDto> _normalAddService, AbstractGenericNormalEditService<PmisContext, LookUpValue, LookUpValueEditRequestDto, LookUpValueEditResponseDto> _normalEditService, AbstractGenericNormalLogicalDeleteService<PmisContext, LookUpValue, LookUpValueDeleteRequestDto, LookUpValueDeleteResponseDto> _logicalDeleteService, AbstractGenericNormalPhysicalDeleteService<PmisContext, LookUpValue, LookUpValueDeleteRequestDto, LookUpValueDeleteResponseDto> _physicalDeleteService, AbstractGenericNormalSearchService<PmisContext, LookUpValue, LookUpValueSearchResponseDto> _searchService, ILookUpDestinationService lookUpDestinationService) : base(_mapper, _normalAddService, _normalEditService, _logicalDeleteService, _physicalDeleteService, _searchService)
+        {
+            this.lookUpDestinationService = lookUpDestinationService;
+        }
+        public async Task<IEnumerable<LookUpDestinationSearchResponseDto>> GetList(string _tableName)
+        {
+            try
+            {
+                GenericSearchRequestDto req = new GenericSearchRequestDto()
+                {
+                    filters = new List<GenericSearchFilterDto>() {
+                        new GenericSearchFilterDto() {
+                            columnName = "TableName",
+                            LogicalOperator = LogicalOperator.Begin,
+                            operation = FilterOperator.Equals,
+                            type = PhraseType.Condition,
+                            value = _tableName
+                        }
+                    }
+                };
+                (bool IsSuccess, IEnumerable<LookUpDestinationSearchResponseDto> list) = await lookUpDestinationService.Search(req);
+               
+                return list;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public async Task<IEnumerable<LookUpValueShortInfoDto>> GetList(string _tableName, string _columnName, string _code)
+        {
+            try
+            {
+                GenericSearchRequestDto req = new GenericSearchRequestDto()
+                {
+                    filters = new List<GenericSearchFilterDto>() {
+                        new GenericSearchFilterDto() {
+                            columnName = "TableName",
+                            LogicalOperator = LogicalOperator.Begin,
+                            operation = FilterOperator.Equals,
+                            type = PhraseType.Condition,
+                            value = _tableName
+                        },
+                        new GenericSearchFilterDto() {
+                            columnName = "ColumnName",
+                            LogicalOperator = LogicalOperator.And,
+                            operation = FilterOperator.Equals,
+                            type = PhraseType.Condition,
+                            value = _columnName
+                        }
+                    }
+                };
+                (bool IsSuccess, IEnumerable<LookUpDestinationSearchResponseDto> list) = await lookUpDestinationService.Search(req);
+                list = list.Where(l => l.FkLookUpInfo.Code == _code);
+
+                return list.Single().LookUpValuesInfo;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public async Task<IEnumerable<LookUpValueShortInfoDto>> GetList(IEnumerable<LookUpDestinationSearchResponseDto> _tablelookUpList, string _columnName, string _code)
+        {
+            try
+            {
+                IEnumerable<LookUpDestinationSearchResponseDto> result = _tablelookUpList.Where(l => l.ColumnName == _columnName && l.FkLookUpInfo.Code == _code && l.FkLookUpInfo.FlgLogicalDelete != true);
+                return await Task.FromResult(result.Single().LookUpValuesInfo.Where(x => x.FlgLogicalDelete != true));
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public async Task<IEnumerable<LookUpShortInfoDto>> GetList(IEnumerable<LookUpDestinationSearchResponseDto> _tablelookUpList, string _code)
+        {
+            try
+            {
+                IEnumerable<LookUpDestinationSearchResponseDto> result = _tablelookUpList.Where(l => l.FkLookUpInfo.Code == _code);
+
+                return await Task.FromResult(result.Select(x => x.FkLookUpInfo));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
+
