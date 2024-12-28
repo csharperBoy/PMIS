c773ee0c70eb0086e79804dddeebf55f0782afad
868 <868@aksteel>
2024-12-28 14:57:23 +0330

    .

--- PMIS/Models/PmisContext.cs
+++ PMIS/Models/PmisContext.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.Extensions.Configuration;
 namespace PMIS.Models;
 
 public partial class PmisContext : DbContext
