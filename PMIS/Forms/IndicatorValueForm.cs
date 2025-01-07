9901031977308efc0cd13b1cc67c85a76fc01de8
868 <868@aksteel>
2025-01-07 21:23:29 +0330

    .

--- PMIS/Forms/IndicatorValueForm.cs
+++ PMIS/Forms/IndicatorValueForm.cs
@@ -1547,7 +1547,17 @@ namespace PMIS.Forms
                             if (isSuccess && lstSearchResponse.Count() > 0)
                             {
                                 dgvResultsList.Rows[index].Cells["Id"].Value = lstSearchResponse.FirstOrDefault().Id;
-                                dgvResultsList.Rows[index].Cells["FlgEdited"].Value = true;
+                                if (
+                                    dgvResultsList.Rows[index].Cells["Value"].Value.ToString() != lstSearchResponse.FirstOrDefault().Value.ToString() ||
+                                    dgvResultsList.Rows[index].Cells["FkLkpValueTypeId"].Value.ToString() != lstSearchResponse.FirstOrDefault().FkLkpValueTypeInfo.Id.ToString() ||
+                                    dgvResultsList.Rows[index].Cells["FkIndicatorId"].Value.ToString() != lstSearchResponse.FirstOrDefault().FkIndicatorInfo.Id.ToString() ||
+                                    dgvResultsList.Rows[index].Cells["FkLkpShiftId"].Value.ToString() != lstSearchResponse.FirstOrDefault().FkLkpShiftInfo.Id.ToString() ||
+                                    dgvResultsList.Rows[index].Cells["shamsiDateTime"].Value.ToString() != lstSearchResponse.FirstOrDefault().shamsiDateTime ||
+                                    dgvResultsList.Rows[index].Cells["Description"].Value.ToString() != lstSearchResponse.FirstOrDefault().Description
+                                    )
+                                {
+                                    dgvResultsList.Rows[index].Cells["FlgEdited"].Value = true;
+                                }
                             }
                             else
                             {
